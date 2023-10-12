fd4ad1554238838fae2f4e56049fe59eeb118b7a
j.seccia <jerome.seccia@digitech.fr>
2023-10-12 17:13:50 +0200

    test reqwest + select

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Cursor;
+
+async fn scrape() -> Result<(), reqwest::Error> {
+    let response = reqwest::get("https://cryptonews.com/").await?;
+    let body = response.text().await?;
+    let cursor = Cursor::new(body);
+    let document = select::document::Document::from_read(cursor).unwrap();
+    for node in document.find(select::predicate::Name("a")) {
+        println!("Link: {}", node.attr("href").unwrap());
+    }
+    Ok(())
 }
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    scrape().await?;
+    Ok(())
+}
+
+

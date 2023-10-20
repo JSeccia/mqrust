f9b83fbf6756a4dec4e9df9dca92436a7887bd65
j.seccia <jerome.seccia@digitech.fr>
2023-10-20 13:58:55 +0200

    testouillage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::io::Cursor;
 
 async fn scrape() -> Result<(), reqwest::Error> {
-    let response = reqwest::get("https://cryptonews.com/").await?;
+    let response = reqwest::get("https://www.boursier.com/indices/composition/cac-40-FR0003500008,FR.html/").await?;
     let body = response.text().await?;
     let cursor = Cursor::new(body);
     let document = select::document::Document::from_read(cursor).unwrap();
-    for node in document.find(select::predicate::Name("a")) {
-        println!("Link: {}", node.attr("href").unwrap());
+    let table = document.find(select::predicate::Class("table table--values table--no-auto")).next();
+    if let Some(table) = table {
+        for row in table.find(select::predicate::Name("tr")) {
+            let cells: Vec<_> = row.find(select::predicate::Name("td"))
+                .map(|cell| cell.text().trim().to_string())
+                .collect();
+            println!("{:?}", cells);
+        }
     }
     Ok(())
 }

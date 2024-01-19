476bc828a7ebee1301acd2b3f83455425774f75d
j.seccia <jerome.seccia@digitech.fr>
2024-01-19 23:22:04 +0100

    docker compose

--- rust_server/src/main.rs
+++ rust_server/src/main.rs
@@ -30,7 +30,7 @@ fn echo(ws: WebSocket) -> Channel<'static> {
                     }
                 },
                 Some(result) = message_stream.next() => {
-                    match & result {
+                    match result {
                         Ok(borrowed_message) => {
                             if let Some(payload) = borrowed_message.payload() {
                                 let text = std::str::from_utf8(payload).unwrap_or("");

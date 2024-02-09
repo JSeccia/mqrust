6a5ade572466bce84ab1e07e6c1bf09ed78b1333
j.seccia <jerome.seccia@digitech.fr>
2024-02-09 11:08:35 +0100

    add react project

---
+++ rust_server/frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import WebSocketComponent from "./WebSocketComponent";
+
+function App() {
+  return (
+   <WebSocketComponent />
+  );
+}
+
+export default App;
